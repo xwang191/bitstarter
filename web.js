8b234f5082df876086ca0b0f9b7ca19344928760
Your Name <you@example.com>
2013-07-05 14:54:07 -0700

    fixed a bug

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var fs = require('fs');
   fs.readFile('./index.html', function (err, data) {
     if (err) throw err;
       console.log(data);
